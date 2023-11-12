753ae8aeb66fb57817b2e63cc314772604aa10e0
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-11-11 19:42:33 -0500

    Added the elements for the insert, queries, and output components . Also organized the buttons, insert, queries and output components into their own divs

--- moviedb/src/app/admin/page.tsx
+++ moviedb/src/app/admin/page.tsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import CreateTables from '@/components/CreateTables'
-import CreateViews from '@/components/CreateViews'
-import PopulateTables from '@/components/PopulateTables'
-import DropTables from '@/components/DropTables'
-import DropViews from '@/components/DropViews'
-import Queries from '@/components/Queries'
-
+import React from "react";
+import CreateTables from "@/components/CreateTables";
+import CreateViews from "@/components/CreateViews";
+import PopulateTables from "@/components/PopulateTables";
+import DropTables from "@/components/DropTables";
+import DropViews from "@/components/DropViews";
+import Queries from "@/components/Queries";
 
 const Admin = () => {
   return (
-    <div>
+    <div className="flex flex-row border-4 border-black">
+      {/* This div is for storing the buttons */}
+      <div className="flex flex-col gap-3 justify-start">
         <CreateTables></CreateTables>
         <CreateViews></CreateViews>
         <PopulateTables></PopulateTables>
         <DropTables></DropTables>
         <DropViews></DropViews>
         <Queries></Queries>
-    </div>
-  )
-}
+      </div>
 
-export default Admin
+      {/* This div is for the inserts */}
+      <div className="">
+        <p></p>
+      </div>
+      
+      {/* This div is for the queries and output elements */}
+      <div className="">
+        <p id="queries">
+
+        </p>
+        
+        <p id="output">
+
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Admin;
