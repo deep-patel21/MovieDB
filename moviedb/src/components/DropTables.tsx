553f0b6d8712ff7e6b8cc503e5a6e32d5612e5f1
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-11-12 22:45:46 -0500

    Accpeted change droptables

--- moviedb/src/components/DropTables.tsx
+++ moviedb/src/components/DropTables.tsx
@@ -5,19 +5,9 @@ import React from "react";
 const DropTables = () => {
 
   const handler = async () => {
-<<<<<<< HEAD
-    try{
-      let request = await fetch("/api/DropTables");
-      const data = await request.json();
-      console.log(data);
-    }catch(err){
-      console.log(err)
-    }
-=======
     let request = await fetch("/api/DropTables");
     const data = await request.json();
     console.log(data);
->>>>>>> 5394bd8bc0926cad92fa2283316494dd61dd48ae
   };
 
   return (
