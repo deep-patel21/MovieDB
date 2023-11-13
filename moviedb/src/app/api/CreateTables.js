12e3ec6891738d260908c8af32854f04d2cff467
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-11-12 22:48:00 -0500

    Successfully connected backend to oracle database

--- moviedb/src/app/api/CreateTables.js
+++ moviedb/src/app/api/CreateTables.js
@@ -1,144 +1,178 @@
-import { getConnection } from 'oracledb';
+const oracledb = require('oracledb');
 
 async function run() {
   let connection;
 
   try {
-    connection = await getConnection({
+    await oracledb.initOracleClient({ libDir: "d:/instantclient_21_12" });
+    connection = await oracledb.getConnection({
       user: 'dcpatel',
       password: '06210050',
-      connectString: 'dcpatel/06210050@oracle.scs.ryerson.ca:1521/orcl',
+      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
     });
 
-    connection.execute(
-           'CREATE TABLE Actor (\
-            actor_id NUMBER PRIMARY KEY,\
-            first_name VARCHAR2(50) NOT NULL,\
-            last_name VARCHAR2(50) NOT NULL,\
-            birthdate DATE,\
-            nationality VARCHAR2(50) NOT NULL,\
-            filmography CLOB\
-        );\
-        CREATE TABLE Director (\
-            director_id NUMBER PRIMARY KEY,\
-            first_name VARCHAR2(50) NOT NULL,\
-            last_name VARCHAR2(50) NOT NULL,\
-            birthdate DATE,\
-            nationality VARCHAR2(50) NOT NULL,\
-            filmography CLOB\
-          );\
-        CREATE TABLE Producer (\
-            producer_id NUMBER PRIMARY KEY,\
-            first_name VARCHAR2(50) NOT NULL,\
-            last_name VARCHAR2(50) NOT NULL,\
-            birthdate DATE,\
-            nationality VARCHAR2(50) NOT NULL,\
-            filmography CLOB\
-        );\
-        CREATE TABLE TheUser (\
-            user_id NUMBER PRIMARY KEY,\
-            first_name VARCHAR2(50) NOT NULL,\
-            last_name VARCHAR2(50) NOT NULL,\
-            username VARCHAR2(50) UNIQUE NOT NULL,\
-            email VARCHAR2(100) UNIQUE NOT NULL,\
-            password VARCHAR2(100) NOT NULL\
-          );\
-        CREATE TABLE Studio (\
-            studio_id NUMBER PRIMARY KEY,\
-            name VARCHAR2(100) NOT NULL,\
-            owner VARCHAR2(50),\
-            location VARCHAR2(200),\
-            credits VARCHAR2(800)\
-        );\
-        CREATE TABLE Receiver (\
-            receiver_id NUMBER PRIMARY KEY,\
-            theName VARCHAR2(50) NOT NULL,\
-            theDate DATE\
-        );\
-        CREATE TABLE Film (\
-            film_id NUMBER PRIMARY KEY,\
-            title VARCHAR2(100) NOT NULL,\
-            runtime NUMBER,\
-            release_year NUMBER,\
-            director_id NUMBER,\
-            FOREIGN KEY (director_id) REFERENCES Director(director_id),\
-            theCast VARCHAR2(1000) NOT NULL\
-          );\
-          CREATE TABLE Review (\
-            review_id NUMBER PRIMARY KEY,\
-            user_id NUMBER,\
-            film_id NUMBER,\
-            theDescription CLOB,\
-            rating NUMBER,\
-            theDate DATE,\
-            FOREIGN KEY (user_id) REFERENCES TheUser(user_id),\
-            FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE Awards (\
-            award_id NUMBER PRIMARY KEY,\
-            theName VARCHAR2(100) NOT NULL,\
-            presenter VARCHAR2(100) NOT NULL,\
-            year_of_win NUMBER,\
-            receiver_id NUMBER,\
-            winner_type VARCHAR2(20) CHECK (winner_type IN (\'Person\', \'Film\')),\
-            FOREIGN KEY (receiver_id) REFERENCES Receiver(receiver_id)\
-          );\
-          CREATE TABLE Actor_Acts_In_Film (\
-            actor_id NUMBER NOT NULL,\
-            film_id NUMBER NOT NULL,\
-            PRIMARY KEY (actor_id, film_id),\
-            FOREIGN KEY (actor_id) REFERENCES Actor(actor_id),\
-            FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE Director_Directs_Film (\
-              director_id NUMBER NOT NULL,\
-              film_id NUMBER NOT NULL,\
-              PRIMARY KEY (director_id, film_id),\
-              FOREIGN KEY (director_id) REFERENCES Director(director_id),\
-              FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE Producer_Produces_Film (\
-              producer_id NUMBER NOT NULL,\
-              film_id NUMBER NOT NULL,\
-              PRIMARY KEY (producer_id, film_id),\
-              FOREIGN KEY (producer_id) REFERENCES Producer(producer_id),\
-              FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE Studio_Owns_Film (\
-              studio_id NUMBER NOT NULL,\
-              film_id NUMBER NOT NULL,\
-              PRIMARY KEY (studio_id, film_id),\
-              FOREIGN KEY (studio_id) REFERENCES Studio(studio_id),\
-              FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE Awards_Given_To_Film (\
-              award_id NUMBER NOT NULL,\
-              film_id NUMBER NOT NULL,\
-              PRIMARY KEY (award_id, film_id),\
-              FOREIGN KEY (award_id) REFERENCES Awards(award_id),\
-              FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );\
-          CREATE TABLE TheUser_Creates_Review (\
-              user_id NUMBER NOT NULL,\
-              review_id NUMBER NOT NULL,\
-              PRIMARY KEY (user_id, review_id),\
-              FOREIGN KEY (user_id) REFERENCES TheUser(user_id),\
-              FOREIGN KEY (review_id) REFERENCES Review(review_id)\
-          );\
-          CREATE TABLE Review_Rates_Film (\
-              review_id NUMBER NOT NULL,\
-              film_id NUMBER NOT NULL,\
-              rating NUMBER,\
-              PRIMARY KEY (review_id, film_id),\
-              FOREIGN KEY (review_id) REFERENCES Review(review_id),\
-              FOREIGN KEY (film_id) REFERENCES Film(film_id)\
-          );');
-    res.status(200).json(result.rows);
+    const createTableStatements = [
+      `CREATE TABLE Actor (
+         actor_id NUMBER PRIMARY KEY,
+         first_name VARCHAR2(50) NOT NULL,
+         last_name VARCHAR2(50) NOT NULL,
+         birthdate DATE,
+         nationality VARCHAR2(50) NOT NULL,
+         filmography CLOB
+       )`,
+
+      `CREATE TABLE Director (
+         director_id NUMBER PRIMARY KEY,
+         first_name VARCHAR2(50) NOT NULL,
+         last_name VARCHAR2(50) NOT NULL,
+         birthdate DATE,
+         nationality VARCHAR2(50) NOT NULL,
+         filmography CLOB
+       )`,
+
+      `CREATE TABLE Producer (
+         producer_id NUMBER PRIMARY KEY,
+         first_name VARCHAR2(50) NOT NULL,
+         last_name VARCHAR2(50) NOT NULL,
+         birthdate DATE,
+         nationality VARCHAR2(50) NOT NULL,
+         filmography CLOB
+       )`,
+
+      `CREATE TABLE TheUser (
+         user_id NUMBER PRIMARY KEY,
+         first_name VARCHAR2(50) NOT NULL,
+         last_name VARCHAR2(50) NOT NULL,
+         username VARCHAR2(50) UNIQUE NOT NULL,
+         email VARCHAR2(100) UNIQUE NOT NULL,
+         password VARCHAR2(100) NOT NULL
+       )`,
+
+      `CREATE TABLE Studio (
+         studio_id NUMBER PRIMARY KEY,
+         name VARCHAR2(100) NOT NULL,
+         owner VARCHAR2(50),
+         location VARCHAR2(200),
+         credits VARCHAR2(800)
+       )`,
+
+      `CREATE TABLE Receiver (
+         receiver_id NUMBER PRIMARY KEY,
+         theName VARCHAR2(50) NOT NULL,
+         theDate DATE
+       )`,
+
+      `CREATE TABLE Film (
+         film_id NUMBER PRIMARY KEY,
+         title VARCHAR2(100) NOT NULL,
+         runtime NUMBER,
+         release_year NUMBER,
+         director_id NUMBER,
+         FOREIGN KEY (director_id) REFERENCES Director(director_id),
+         theCast VARCHAR2(1000) NOT NULL
+       )`,
+
+      `CREATE TABLE Review (
+         review_id NUMBER PRIMARY KEY,
+         user_id NUMBER,
+         film_id NUMBER,
+         theDescription CLOB,
+         rating NUMBER,
+         theDate DATE,
+         FOREIGN KEY (user_id) REFERENCES TheUser(user_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE Awards (
+         award_id NUMBER PRIMARY KEY,
+         theName VARCHAR2(100) NOT NULL,
+         presenter VARCHAR2(100) NOT NULL,
+         year_of_win NUMBER,
+         receiver_id NUMBER,
+         winner_type VARCHAR2(20) CHECK (winner_type IN ('Person', 'Film')),
+         FOREIGN KEY (receiver_id) REFERENCES Receiver(receiver_id)
+       )`,
+
+      `CREATE TABLE Actor_Acts_In_Film (
+         actor_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         PRIMARY KEY (actor_id, film_id),
+         FOREIGN KEY (actor_id) REFERENCES Actor(actor_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE Director_Directs_Film (
+         director_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         PRIMARY KEY (director_id, film_id),
+         FOREIGN KEY (director_id) REFERENCES Director(director_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE Producer_Produces_Film (
+         producer_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         PRIMARY KEY (producer_id, film_id),
+         FOREIGN KEY (producer_id) REFERENCES Producer(producer_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE Studio_Owns_Film (
+         studio_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         PRIMARY KEY (studio_id, film_id),
+         FOREIGN KEY (studio_id) REFERENCES Studio(studio_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE Awards_Given_To_Film (
+         award_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         PRIMARY KEY (award_id, film_id),
+         FOREIGN KEY (award_id) REFERENCES Awards(award_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+
+      `CREATE TABLE TheUser_Creates_Review (
+         user_id NUMBER NOT NULL,
+         review_id NUMBER NOT NULL,
+         PRIMARY KEY (user_id, review_id),
+         FOREIGN KEY (user_id) REFERENCES TheUser(user_id),
+         FOREIGN KEY (review_id) REFERENCES Review(review_id)
+       )`,
+
+      `CREATE TABLE Review_Rates_Film (
+         review_id NUMBER NOT NULL,
+         film_id NUMBER NOT NULL,
+         rating NUMBER,
+         PRIMARY KEY (review_id, film_id),
+         FOREIGN KEY (review_id) REFERENCES Review(review_id),
+         FOREIGN KEY (film_id) REFERENCES Film(film_id)
+       )`,
+    ];
+
+    const options = {
+      autoCommit: true
+    };
+
+    for (const statement of createTableStatements) {
+      try {
+        await connection.execute(statement);
+        console.log(`Table created: ${statement}`);
+      } catch (err) {
+        console.error(`Error creating table: ${statement}`, err);
+      }
+    }
+
+    console.log('All tables created successfully.');
+
+    // Close the connection only once after all statements are executed
+    await connection.close();
 
   } catch (err) {
     console.error(err);
   } finally {
-    if (connection) {
+    if (connection && connection.getConnectionStatus() === oracledb.CONNECTION_STATUS_CONNECTED) {
       try {
         await connection.close();
       } catch (err) {
--- moviedb/src/app/api/DropTables.js
+++ moviedb/src/app/api/DropTables.js
@@ -4,32 +4,46 @@ async function run() {
   let connection;
 
   try {
+    await oracledb.initOracleClient({ libDir: "d:/instantclient_21_12"});
     connection = await oracledb.getConnection({
       user: 'dcpatel',
       password: '06210050',
-      connectString: 'localhost:1521/orcl',
+      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
     });
 
-    const result = await connection.execute(" \
-      DROP TABLE Actor CASCADE CONSTRAINTS; \
-      DROP TABLE Director CASCADE CONSTRAINTS; \
-      DROP TABLE Producer CASCADE CONSTRAINTS; \
-      DROP TABLE TheUser CASCADE CONSTRAINTS; \
-      DROP TABLE Studio CASCADE CONSTRAINTS; \
-      DROP TABLE Receiver CASCADE CONSTRAINTS; \
-      DROP TABLE Film CASCADE CONSTRAINTS; \
-      DROP TABLE Review CASCADE CONSTRAINTS; \
-      DROP TABLE Awards CASCADE CONSTRAINTS; \
-      DROP TABLE Actor_Acts_In_Film CASCADE CONSTRAINTS; \
-      DROP TABLE Director_Directs_Film CASCADE CONSTRAINTS; \
-      DROP TABLE Producer_Produces_Film CASCADE CONSTRAINTS; \
-      DROP TABLE Studio_Owns_Film CASCADE CONSTRAINTS; \
-      DROP TABLE Awards_Given_To_Film CASCADE CONSTRAINTS; \
-      DROP TABLE TheUser_Creates_Review CASCADE CONSTRAINTS; \
-      DROP TABLE Review_Rates_Film CASCADE CONSTRAINTS; ");
-    
-    //res.status(200).json(result.rows);
+    const statements = [ "DROP TABLE Actor CASCADE CONSTRAINTS",
+                        "DROP TABLE Director CASCADE CONSTRAINTS",
+                        "DROP TABLE Producer CASCADE CONSTRAINTS",
+                        "DROP TABLE TheUser CASCADE CONSTRAINTS",
+                        "DROP TABLE Studio CASCADE CONSTRAINTS",
+                        "DROP TABLE Receiver CASCADE CONSTRAINTS",
+                        "DROP TABLE Film CASCADE CONSTRAINTS",
+                        "DROP TABLE Review CASCADE CONSTRAINTS",
+                        "DROP TABLE Awards CASCADE CONSTRAINTS",
+                        "DROP TABLE Actor_Acts_In_Film CASCADE CONSTRAINTS",
+                        "DROP TABLE Director_Directs_Film CASCADE CONSTRAINTS",
+                        "DROP TABLE Producer_Produces_Film CASCADE CONSTRAINTS",
+                        "DROP TABLE Studio_Owns_Film CASCADE CONSTRAINTS",
+                        "DROP TABLE Awards_Given_To_Film CASCADE CONSTRAINTS",
+                        "DROP TABLE TheUser_Creates_Review CASCADE CONSTRAINTS",
+                        "DROP TABLE Review_Rates_Film CASCADE CONSTRAINTS"];
 
+    const options = {
+      autoCommit: true
+    }
+
+    for (const statement of statements) {
+      try {
+        await connection.execute(statement);
+      } catch (err) {
+          console.log(err);
+      }
+    }
+
+    //const result = await connection.execute(statements)
+   // res.status(200).json(result.rows);
+
+    await connection.close()
   } catch (err) {
     console.error(err);
   } finally {
