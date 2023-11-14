40d5f416f613afddecf51f43b2e33224957f7295
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-11-13 20:51:26 -0500

    Validated version of DropViews.js and CreateViews.js

--- moviedb/src/app/api/DropViews.js
+++ moviedb/src/app/api/DropViews.js
@@ -4,17 +4,30 @@ async function run() {
   let connection;
 
   try {
+    oracledb.initOracleClient({ libDir: "d:/instantclient_21_12" });
     connection = await oracledb.getConnection({
       user: 'dcpatel',
       password: '06210050',
-      connectString: 'dcpatel/06210050@oracle.scs.ryerson.ca:1521/orcl',
+      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
     });
 
-    const result = await connection.execute(" \
-      DROP VIEW Film_View_without_Director; \
-      DROP VIEW Actor_Notable_Works; \
-      DROP VIEW Anonymous_Ratings; ");
-    res.status(200).json(result.rows);
+    const dropViewStatements = ["DROP VIEW Film_View_without_Director",
+                                "DROP VIEW Actor_Notable_Works",
+                                "DROP VIEW Anonymous_Ratings"];
+
+    
+    for (const statement of dropViewStatements) {
+      try {
+        await connection.execute(statement);
+        console.log(`View Dropped: ${statement}`);
+      } catch (err) {
+        console.error(`Error Dropping View: ${statement}`, err);
+      }
+    }
+
+    console.log("All views dropped successfully.")
+
+    //res.status(200).json(result.rows);
 
   } catch (err) {
     console.error(err);
