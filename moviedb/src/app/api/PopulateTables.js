afe99544b2d5f2674e85bc795ed48d1db52b660d
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-11-14 17:50:28 -0500

    all preset queries hardcoded into Queries.js

--- moviedb/src/app/api/PopulateTables.js
+++ moviedb/src/app/api/PopulateTables.js
@@ -4,19 +4,19 @@ async function run() {
   let connection;
 
   try {
-    // oracledb.initOracleClient({ libDir:  "d:/instantclient_21_12"});
-    oracledb.initOracleClient({ libDir: "C:/Users/pra_d/OneDrive/Desktop/Anmol/Ryerson/instantclient-basic-windows.x64-21.12.0.0.0dbru/instantclient_21_12" });
-    connection = await oracledb.getConnection({
-      user: 'appancha',
-      password: '11157696',
-      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
-    });
+    //oracledb.initOracleClient({ libDir: "C:/Users/pra_d/OneDrive/Desktop/Anmol/Ryerson/instantclient-basic-windows.x64-21.12.0.0.0dbru/instantclient_21_12" });
+    //connection = await oracledb.getConnection({
+    //  user: 'appancha',
+    //  password: '11157696',
+    //  connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
+    //});
 
-    // connection = await oracledb.getConnection({
-    //   user: 'dcpatel',
-    //   password: '06210050',
-    //   connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
-    // });
+    oracledb.initOracleClient({ libDir:  "d:/instantclient_21_12"});
+     connection = await oracledb.getConnection({
+       user: 'dcpatel',
+       password: '06210050',
+       connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
+     });
 
 
     const actorInsertStatements = [
@@ -37,7 +37,7 @@ async function run() {
     for (const statement of actorInsertStatements) {
         try {
             await connection.execute(statement);
-            //console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -60,7 +60,7 @@ async function run() {
        for (const statement of directorInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -79,7 +79,7 @@ async function run() {
         for (const statement of producerInsertStatements) {
             try {
                 await connection.execute(statement);
-                // console.log(`Data Inserted for: ${statement}`);
+                console.log(`Data Inserted for: ${statement}`);
             } catch (err) {
                 console.error(`Error inserting data: ${statement}`, err);
             }
@@ -100,7 +100,7 @@ async function run() {
         for (const statement of userInsertStatements) {
             try {
                 await connection.execute(statement);
-                // console.log(`Data Inserted for: ${statement}`);
+                console.log(`Data Inserted for: ${statement}`);
             } catch (err) {
                 console.error(`Error inserting data: ${statement}`, err);
             }
@@ -120,7 +120,7 @@ async function run() {
       for (const statement of studioInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -140,7 +140,7 @@ async function run() {
        for (const statement of receiverInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -163,7 +163,7 @@ async function run() {
       for (const statement of filmInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -182,7 +182,7 @@ async function run() {
       for (const statement of awardsInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
@@ -205,14 +205,14 @@ async function run() {
     for (const statement of reviewInsertStatements) {
         try {
             await connection.execute(statement);
-            // console.log(`Data Inserted for: ${statement}`);
+            console.log(`Data Inserted for: ${statement}`);
         } catch (err) {
             console.error(`Error inserting data: ${statement}`, err);
         }
       }
 
-    //res.status(200).json(result.rows); */
     await connection.execute("COMMIT")
+    //res.status(200).json(result.rows); */
   } catch (err) {
     console.error(err);
   } finally {
--- moviedb/src/app/api/Queries.js
+++ moviedb/src/app/api/Queries.js
@@ -1,30 +1,37 @@
+const { connect } = require('http2');
 const oracledb = require('oracledb');
 
 async function queries() {
   let connection;
 
   try {
-    // oracledb.initOracleClient({ libDir:  "d:/instantclient_21_12"});
-    oracledb.initOracleClient({ libDir: "C:/Users/pra_d/OneDrive/Desktop/Anmol/Ryerson/instantclient-basic-windows.x64-21.12.0.0.0dbru/instantclient_21_12" });
-    connection = await oracledb.getConnection({
-      user: 'appancha',
-      password: '11157696',
-      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
-    });
+    //oracledb.initOracleClient({ libDir: "C:/Users/pra_d/OneDrive/Desktop/Anmol/Ryerson/instantclient-basic-windows.x64-21.12.0.0.0dbru/instantclient_21_12" });
+   // connection = await oracledb.getConnection({
+   //   user: 'appancha',
+   //   password: '11157696',
+   //   connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
+   // });
 
-    // connection = await oracledb.getConnection({
-    //   user: 'dcpatel',
-    //   password: '06210050',
-    //   connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
-    // });
-
+    oracledb.initOracleClient({ libDir:  "d:/instantclient_21_12"});
+     connection = await oracledb.getConnection({
+       user: 'dcpatel',
+       password: '06210050',
+       connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))',
+     });
 
     try {
       const systemTestQuery = await connection.execute('SELECT SYSDATE FROM DUAL');
-      console.log("Rows: ", systemTestQuery.rows);
       console.log(" ");
-      const query1 = await connection.execute('SELECT f.title AS title, f.runtime AS RUNTIME, d.first_name || \' \' || d.last_name AS DIRECTOR_and_MOVIE FROM Film f JOIN Director d ON f.director_id = d.director_id WHERE runtime > 120 ORDER BY first_name');
-      console.log(query1.rows)
+
+      console.log("Rows: ", systemTestQuery.rows);
+      const query1 = await connection.execute('SELECT f.title AS title, d.first_name || \' \' || d.last_name AS DIRECTOR_and_MOVIE FROM Film f JOIN Director d ON f.director_id = d.director_id ORDER BY first_name');
+      const query2 = await connection.execute('SELECT first_name, last_name FROM Actor');
+      const query3 = await connection.execute('SELECT first_name, last_name, username, password FROM TheUser ORDER BY password');
+      const query4 = await connection.execute('SELECT a.theName, a.presenter, a.year_of_win, r.theName FROM Awards a JOIN Receiver r ON a.receiver_id = r.receiver_id WHERE a.year_of_win > 2021');
+      console.log("Query Result 1:", query1.rows);
+      console.log("Query Result 2:", query2.rows);
+      console.log("Query Result 3:", query3.rows);
+      console.log("Query Result 4:", query4.rows);
 
     } catch (err) {
       console.log(err);
